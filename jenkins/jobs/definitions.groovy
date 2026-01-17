da66b534cc297cf7f2fc2d7728457f7d51aece92
Maciej Gurgul <maciejgurgul01@gmail.com>
2026-01-17 16:08:24 +0100

    test jenkinsfile

--- jenkins/jobs/definitions.groovy
+++ jenkins/jobs/definitions.groovy
@@ -9,7 +9,7 @@ pipelineJob('ansible-folder/deploy-job') {
                         url('https://github.com/Margulwb/update-os.git')
                         credentials('github-token')
                     }
-                    branches('feature/jenkinsfile-generate')
+                    branches('feature/*')
                 }
             }
             scriptPath('Jenkinsfile.ansible-playbook')
